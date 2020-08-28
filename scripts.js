6bb2b8005ef776ef30d27ccba95435beb0932cb8
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-27 19:21:27 -0500

    captured click events

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$( document ).ready( readyNow );
+
+function readyNow(){
+    $( '.redButton' ).on( 'click', redSquareDisplay );
+    $( '.blueButton' ).on( 'click', blueSquareDisplay );
+    $( '.greenButton' ).on( 'click', greenSquareDisplay );
+    $( '.yellowButton' ).on( 'click', yellowSquareDisplay );
+} // end readyNow
+
+function redSquareDisplay(){
+    
+} // end redSquareDisplay
+
+function blueSquareDisplay(){
+
+} // end blueSquareDisplay
+
+function greenSquareDisplay(){
+
+} // end greenSquareDisplay
+
+function yellowSquareDisplay(){
+
+} // end yellowSquareDisplay
