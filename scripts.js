98f621f0ef129969361ea6de9698e96301bfa748
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-27 21:28:42 -0500

    got color squares to work on click event

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+/*
+[x] - Add 4 buttons
+[] - when clicked, add color block to DOM
+[] - count total of each block
+*/
+
+
 $( document ).ready( readyNow );
 
 function readyNow(){
@@ -7,18 +14,34 @@ function readyNow(){
     $( '.yellowButton' ).on( 'click', yellowSquareDisplay );
 } // end readyNow
 
+let redNumber = 1;
 function redSquareDisplay(){
-    
+    $( '.squares' ).append( '<div class=redSquare></div>' );
+    let el = $( '.redCount' );
+    el.empty();
+    el.append( ' ', redNumber++ );
 } // end redSquareDisplay
 
+let blueNumber = 1;
 function blueSquareDisplay(){
-
+    $( '.squares' ).append( '<div class=blueSquare></div>' );
+    let el = $( '.blueCount' );
+    el.empty();
+    el.append( ' ', blueNumber++ );
 } // end blueSquareDisplay
 
+let greenNumber = 1;
 function greenSquareDisplay(){
-
+    $( '.squares' ).append( '<div class=greenSquare></div>' );
+    let el = $( '.greenCount' );
+    el.empty();
+    el.append( ' ', greenNumber++ );
 } // end greenSquareDisplay
 
+let yellowNumber = 1
 function yellowSquareDisplay(){
-
+    $( '.squares' ).append( '<div class=yellowSquare></div>' );
+    let el = $( '.yellowCount' );
+    el.empty();
+    el.append( ' ', yellowNumber++ );
 } // end yellowSquareDisplay
